a44088463292a647cdc5e77e24b06388990b3f89
bjiorken <bjiorken@gmail.com>
2019-02-22 16:30:35 -0600

    Set debug to default to false. The user can change it to whatever they want, but most of the time the onboard LEDs are sufficient for debugging. With recent versions of Arduino the F() macro causes compile errors, with debugging turned off it should compile ok. In time Arduino will fix the issue and then it won't matter either way.

--- OpenSourceLights/AA_UserConfig.h
+++ OpenSourceLights/AA_UserConfig.h
@@ -112,7 +112,7 @@
 
 // DEBUGGING
 // ------------------------------------------------------------------------------------------------------------------------------------------------>
-    #define DEBUG                     true          // Set this to true to receive debugging messages out the serial port. NOTE: This will make the program less responsive, 
+    #define DEBUG                     false         // Set this to true to receive debugging messages out the serial port. NOTE: This will make the program less responsive, 
                                                     // so turn it off when you are done troubleshooting. 
     #define LED_DEBUG                 true          // If this is set to true, the Green LED on the board will be on whenever the car is moving forward, 
                                                     // the Red LED will come on whenever the car is moving in reverse, 
--- OpenSourceLights/OpenSourceLights.ino
+++ OpenSourceLights/OpenSourceLights.ino
@@ -1,7 +1,7 @@
  /*
  * Open Source Lights   An Arduino-based program to control LED lights on RC vehicles. 
  * Version:             2.21
- * Last Updated:        02/20/2019
+ * Last Updated:        02/22/2019
  * Copyright 2011       Luke Middleton
  *
  * For more information, see the RCGroups thread: 
