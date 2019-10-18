20a6de68c099ffaf6700a60cf3ef1f0f09f7c6d6
bjiorken <bjiorken@gmail.com>
2019-10-17 19:12:52 -0500

    Update to version 2.24

--- OpenSourceLights/OpenSourceLights.ino
+++ OpenSourceLights/OpenSourceLights.ino
@@ -1,7 +1,7 @@
  /*
  * Open Source Lights   An Arduino-based program to control LED lights on RC vehicles. 
- * Version:             2.23
- * Last Updated:        06/01/2019
+ * Version:             2.24
+ * Last Updated:        10/17/2019
  * Copyright 2011       Luke Middleton
  *
  * For more information, see the RCGroups thread: 
